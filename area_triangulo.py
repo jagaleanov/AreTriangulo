08b9c5cfe536e8d2a29b2b6a5178911de8cf65f5
jagaleanov <54491111+jagaleanov@users.noreply.github.com>
2020-07-20 01:12:51 -0500

    Add files via upload

---
+++ area_triangulo.py
@@ -0,0 +1,7 @@
+
+# Escribir un programa que calcule el área de un triángulo,
+# capturando los valores de base y altura
+
+base = int(input("longitud de base del triangulo: "))
+altura = int(input("longitud de altura del triangulo: "))
+print("El area del triangulo es ",((base * altura) / 2))
